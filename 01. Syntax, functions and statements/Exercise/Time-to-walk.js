8c90d02cb440198e3371a1c62947a1464f3ee679
Bozhikov99 <registracia103@abv.bg>
2021-05-23 23:15:33 +0300

    Adding.

--- 01. Syntax, functions and statements/Exercise/Time-to-walk.js
+++ 01. Syntax, functions and statements/Exercise/Time-to-walk.js
@@ -1,15 +1,18 @@
 function solve(stepInput, footprintInput, speedInput)
 {
-    let steps=stepInput;
-    let footprint=footprintInput;
-    let speed=speedInput*1000/3600;
+    let speed=speedInput/3.6;
 
-    let distance=Number(steps)*Number(footprint);
+    let distance=stepInput*footprintInput;
     let rests=Math.trunc(Number(distance/500));
 
     let time=distance/speed+rests*60;
-    
-    console.log(time);
+
+    let hours=Math.trunc(time/3600);
+    let minutes=Math.trunc(time/60);
+    let seconds=Math.trunc(time%60);
+
+    console.log(`${hours.toString().padStart(2,0)}:${minutes.toString().padStart(2,0)}:${seconds.toString().padStart(2,0)}`)
 }
 
 solve(4000, 0.60, 5);
+//solve(2564, 0.70, 5.5);
