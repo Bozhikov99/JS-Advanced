f7045186ed41a3edd81bdad83a9fb0c1f3a12c90
Bozhikov99 <registracia103@abv.bg>
2021-05-28 00:18:59 +0300

    Adding exercise.

--- 02. Arrays and nested arrays/Exercise/List-of-names.js
+++ 02. Arrays and nested arrays/Exercise/List-of-names.js
@@ -1,6 +1,5 @@
 function solve(input){
-    input.sort((a, b)=>a[0]-b[0])
-    .sort((a, b)=>a[1]-b[1]);
+    input.sort((a, b)=>a.localeCompare(b));
 
     for (let i = 0; i < input.length; i++) {
         console.log(`${i+1}.${input[i]}`);
