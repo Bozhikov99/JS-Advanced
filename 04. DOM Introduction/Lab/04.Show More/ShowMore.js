840d93d5070bce8e8d2141347dbbb6de84ed0c1a
Bozhikov99 <registracia103@abv.bg>
2021-06-03 20:26:51 +0300

    Adding exercise.

---
+++ 04. DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+    let linkElement=document.getElementById('more');
+    let textElement=document.getElementById('text');
+    
+    linkElement.style.display='none';
+    textElement.style.display='inline';
+
+}
