262447c3c4aa812cbf738aa520ae66fd11526017
Bozhikov99 <registracia103@abv.bg>
2021-06-17 11:12:22 +0300

    Adding exercise.

--- 08. Classes/Exercise/05.-Length-limit.js
+++ 08. Classes/Exercise/05.-Length-limit.js
@@ -23,13 +23,13 @@ class Stringer{
 
     toString(){
         
-        this.innerString=this.initialString.slice(0, this.innerLength);
-
+        
         if (this.innerLength==0) {
             return '...';
         }
         
-        if (this.innerString.length<this.innerLength) {
+        if (this.innerString.length>this.innerLength) {
+            this.innerString=this.initialString.slice(0, this.innerLength);
             return this.innerString+'...';
         }
 
@@ -37,11 +37,12 @@ class Stringer{
     }
 
 }
-let test = new Stringer("Test", 5);
-console.log(test.toString()); // Test
-test.decrease(3);
-console.log(test.toString()); // Te...
-test.decrease(5);
-console.log(test.toString()); // ...
-test.increase(4); 
-console.log(test.toString());
+
+ let test = new Stringer("Test", 5);
+ console.log(test.toString()); // Test
+ test.decrease(3);
+ console.log(test.toString()); // Te...
+ test.decrease(5);
+ console.log(test.toString()); // ...
+ test.increase(4); 
+ console.log(test.toString());
