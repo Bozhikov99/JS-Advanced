a9b84a0f8a53c0656674771f48c754b4ffc495f7
Bozhikov99 <registracia103@abv.bg>
2021-06-17 12:34:48 +0300

    Adding exercise.

--- 08. Classes/Exercise/07.-Hex.js
+++ 08. Classes/Exercise/07.-Hex.js
@@ -8,32 +8,7 @@ class Hex {
     }
 
     toString() {
-
-        let decimal = this.value;
-        let hexValues = {
-            10: 'A',
-            11: 'B',
-            12: 'C',
-            13: 'D',
-            14: 'E',
-            15: 'F'
-        }
-
-        let convertedValue = '0x';
-
-        while (decimal > 0) {
-
-            let current = decimal % 16;
-
-            if (hexValues[current]) {
-                current = hexValues[current];
-            }
-
-            decimal = Math.floor(decimal / 16);
-
-            convertedValue += current;
-        }
-        return convertedValue;
+        return `0x${this.value.toString(16).toUpperCase()}`;
     }
 
     plus(num) {
@@ -45,33 +20,7 @@ class Hex {
     }
 
     parse(hex) {
-
-        let chars = hex.substring(2);
-
-        let hexValues = {
-            "A": '10',
-            "B": '11',
-            "C": '12',
-            "D": '13',
-            "E": '14',
-            "F": '15'
-        }
-
-        let parsed = 0;
-
-        for (let i = chars.length - 1; i >= 0; i--) {
-            let current = chars[i];
-
-            if (hexValues[current]) {
-                current=hexValues[current];
-            }
-
-            current=Number(current);
-
-            parsed+=current*16**i;
-        }
-
-        return parsed
+        return parseInt(hex, 16);
     }
 
 }
