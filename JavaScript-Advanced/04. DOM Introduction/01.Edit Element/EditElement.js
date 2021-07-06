b5c8ee62a0eb00d034af0af02425036072c1fdc1
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-07-06 19:02:10 +0300

    fix

--- JavaScript-Advanced/04. DOM Introduction/01.Edit Element/EditElement.js
+++
@@ -1,6 +0,0 @@
-function editElement(elementInput, match, replacerInput) {
-    const text=elementInput.textContent;
-    const matcher=new RegExp(match, 'g');
-    const edited=text.replace(matcher, replacerInput);
-    elementInput.textContent=edited;
-}
--- JavaScript-Advanced/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++
@@ -1,7 +0,0 @@
-function colorize() {
-    let rows=document.querySelectorAll('table tr:nth-of-type(2n)');
-
-    for (const r of rows) {
-        r.style.background="teal";
-    }
-}
