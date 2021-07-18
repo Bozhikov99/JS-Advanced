e64e4d7dcce7b3062338753d4e572d065355d7f1
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2021-07-18 22:22:44 +0300

    Adding homework.

---
+++ JS-Apps/02. Asynchronous Programming/Exercise/06.Blog/app.js
@@ -0,0 +1,72 @@
+function attachEvents() {
+
+    let loadButtonElement = document.getElementById('btnLoadPosts');
+    let viewButtonElement = document.getElementById('btnViewPost');
+    loadButtonElement.addEventListener('click', getOptions);
+    viewButtonElement.addEventListener('click', getPost);
+
+    let posts = {};
+
+    async function getOptions() {
+        let url = 'http://localhost:3030/jsonstore/blog/posts';
+        let postsRequest = await fetch(url);
+        let postsObject = await postsRequest.json();
+        let selectElement = document.getElementById('posts');
+        Array.from(selectElement.children)
+            .forEach(c => c.remove());
+
+        for (const p of Object.values(postsObject)) {
+            selectElement.appendChild(createOption(p));
+        }
+    }
+
+    function createOption(post) {
+        let title = post.title;
+        let id = post.id;
+        let body = post.body;
+
+        posts[id] = {
+            title,
+            body
+        };
+        let optionElement = document.createElement('option');
+        optionElement.value = id;
+        optionElement.textContent = title;
+
+        return optionElement;
+    }
+
+    async function getPost() {
+
+        let selectElement = document.getElementById('posts');
+        let currentPostKey = selectElement.value;
+
+        let url = `http://localhost:3030/jsonstore/blog/comments`;
+        let commentsRequest = await fetch(url);
+        let commentsObject = await commentsRequest.json();
+
+        let comments = Array.from(Object.values(commentsObject))
+            .filter(c => c.postId == currentPostKey);
+
+        let titleH1Element = document.getElementById('post-title');
+        titleH1Element.textContent = posts[currentPostKey].title.toUpperCase();
+
+        document.getElementById('post-body')
+            .textContent = posts[currentPostKey].body;
+
+        let commentBoxElement = document.getElementById('post-comments');
+
+        comments.forEach(c => {
+            let text = c.text;
+            let id = c.id;
+
+            let liElement = document.createElement('li');
+            liElement.id = id;
+            liElement.textContent = text;
+
+            commentBoxElement.appendChild(liElement);
+        });
+    }
+}
+
+attachEvents();
